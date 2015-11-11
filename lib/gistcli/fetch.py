e98163edcfafd73659e00a92721e5dc7f084d922
holly <emperor.kurt@gmail.com>
2015-11-11 16:40:49 +0900

    add show and fetch module

--- lib/gistcli/fetch.py
+++ lib/gistcli/fetch.py
@@ -6,36 +6,18 @@ import pprint
 import time
 import warnings
 import os, sys, io
-import stat
 import re
+import subprocess
 import gistcli.show 
 
 class Cli(gistcli.show.Cli):
 
-    def execute(self):
+    GIT_CMD = '/usr/bin/git'
 
-        gist = self.gist_single_from_name()
-        content = self.content_from_gist(gist)
+    def content_from_gist(self, gist):
 
-        path = None
-        if self.args.remote_name:
-            path = gist['filename']
-            with open(path, 'w') as f:
-                f.write(content)
-        elif self.args.output:
-            path = self.args.output.name
-            self.args.output.write(content)
-            self.args.output.close()
-        else:
-            raise Exception("must be specified the option -O( --remote-name) or -o( --output=)FILE_NAME")
-
-        self.add_executable(path)
-
-    def add_executable(self, path, force=False):
-
-        if self.args.add_executable or force is True:
-            st = os.stat(path)
-            # see http://docs.python.jp/3.4/library/stat.html
-            #os.chmod(path, st.st_mode|stat.S_IEXEC)
-            os.chmod(path, st.st_mode|stat.S_IXUSR|stat.S_IXGRP|stat.S_IXOTH)
+        cmd = [ self.__class__.GIT_CMD, "clone", gist["git_pull_url"]]
+        if self.args.download_dir:
+            cmd.append(self.args.download_dir)
+        subprocess.check_call(cmd)
 
--- lib/gistcli/show.py
+++ lib/gistcli/show.py
@@ -11,21 +11,17 @@ from gistcli.skel import Skel
 
 class Cli(Skel):
 
-    GIST_API_URL = 'https://api.github.com/users/{0}/gists'
-    # ex: <https://api.github.com/gists?page=4>; rel="next", <https://api.github.com/gists?page=100>; rel="last", <https://api.github.com/gists?page=1>; rel="first", <https://api.github.com/gists?page=2>; rel="prev"
-    LINK_COMPILE_PETTERN = re.compile("^<(.*)>; rel=\"next\".*")
+    GIST_API_URL = 'https://api.github.com/gists/{0}'
 
     def execute(self):
 
-        gist = self.gist_single_from_name()
-        print(self.content_from_gist(gist))
+        gist = self.gist_single()
+        self.content_from_gist(gist)
 
-    def gist_single_from_name(self, url=None):
+    def gist_single(self, url=None):
         # list gits
         if url is None:
-            url = self.__class__.GIST_API_URL.format(self.args.user)
-
-        next_url = None
+            url = self.__class__.GIST_API_URL.format(self.args.id)
 
         self.verbose_message("request url:{0}".format(url))
 
@@ -33,38 +29,17 @@ class Cli(Skel):
         headers = res.getheaders()
         body    = res.read().decode("utf-8")
 
-        for (key, value) in headers:
-            if key == 'Status' and value != "200 OK":
-                self.verbose_message("body: {0}".format(body))
-                raise Exception("Status: {0}".format(value))
-            if key == 'Link':
-                m = self.__class__.LINK_COMPILE_PETTERN.match(value)
-                if m:
-                    next_url = m.group(1)
-                    self.verbose_message("Link header and next_url[{0}] are exists".format(next_url))
-
-        gists = self.json_loads(body)
-
-        for gist in gists:
-            if self.args.name in gist['files']:
-                return gist['files'][self.args.name]
-
-        # recursive execution
-        if next_url:
-            self.verbose_message("gist_single recursive execution:{0}".format(next_url))
-            time.sleep(1)
-            return self.gist_single_from_name(next_url)
-
-        # nothing
-        raise Exception("user:{0} or file name:{1} is not exists".format(self.args.user, self.args.name))
+        return self.json_loads(body)
 
     def content_from_gist(self, gist):
 
-        raw_url = gist['raw_url']
-        res = self.make_response(gist["raw_url"])
-        headers = res.getheaders()
-        for (key, value) in headers:
-            if key == 'Status' and value != "200 OK":
-                raise Exception("Status: {0}".format(value))
-        return res.read().decode("utf-8")
+
+        for key in gist["files"]:
+            filename = gist["files"][key]["filename"]
+            content = gist["files"][key]["content"]
+
+            
+            print("##### {0} #####".format(filename))
+            print(content)
+            print()
 
